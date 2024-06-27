487ec33d31a186282866b3f757a127c34de94c6f
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-06-27 19:30:27 +0100

    Simplify handling multi-line strings

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,26 @@
 fn main() {
     let string = "Scything isn't sorry";
 
-    let mut indicators = vec![Vec::new()];
-    let mut line_count: usize = 0;
+    let mut indicators = Vec::new();
 
     for indicator in string.chars() {
         if indicator.is_ascii_alphabetic() {
-            indicators[line_count].push(format!(
+            indicators.push(format!(
                 ":regional_indicator_{}: ",
                 indicator.to_lowercase()
             ));
         } else if indicator.is_numeric() {
-            indicators[line_count].push(format!(":{indicator}:"));
+            indicators.push(format!(":{indicator}:"));
         } else if indicator.is_whitespace() && !indicator.eq(&'\n') {
-            indicators[line_count].push(" ".to_string());
+            indicators.push(" ".to_string());
         } else if indicator.eq(&'\n') {
-            line_count += 1;
-            indicators.push(Vec::new())
+            indicators.push('\n'.to_string())
         } else if indicator.eq(&'!') {
-            indicators[line_count].push(":exclamation: ".to_string());
+            indicators.push(":exclamation: ".to_string());
         } else if indicator.eq(&'?') {
-            indicators[line_count].push(":question: ".to_string());
+            indicators.push(":question: ".to_string());
         }
     }
 
-    for discord in indicators {
-        println!("{}", discord.join("").trim_end());
-    }
+    println!("{}", indicators.join("").trim_end());
 }
