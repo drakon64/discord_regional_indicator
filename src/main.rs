f8b2656b69d0597b6df491d6038f919fa7655ea9
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-06-27 23:41:20 +0100

    Remove error handling

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,42 @@
 use std::env::args;
-use std::process::ExitCode;
 
-fn main() -> ExitCode {
-    let args = args().skip(1).next();
+fn main() {
+    let mut indicators = Vec::new();
 
-    if args.is_some() {
-        let mut indicators = Vec::new();
+    for character in args().skip(1).next().unwrap().chars() {
+        if character.is_ascii_alphabetic() {
+            indicators.push(format!(
+                ":regional_indicator_{}: ",
+                character.to_lowercase()
+            ));
+        } else if character.is_numeric() {
+            let number;
 
-        for character in args.unwrap().chars() {
-            if character.is_ascii_alphabetic() {
-                indicators.push(format!(
-                    ":regional_indicator_{}: ",
-                    character.to_lowercase()
-                ));
-            } else if character.is_numeric() {
-                let number;
-
-                match character as u8 - '0' as u8 {
-                    0 => number = "zero",
-                    1 => number = "one",
-                    2 => number = "two",
-                    3 => number = "three",
-                    4 => number = "four",
-                    5 => number = "five",
-                    6 => number = "six",
-                    7 => number = "seven",
-                    8 => number = "eight",
-                    9 => number = "nine",
-                    _ => panic!(), // Unreachable
-                }
-
-                indicators.push(format!(":{number}: "));
-            } else if character.is_whitespace() && !character.eq(&'\n') {
-                indicators.push(" ".to_string());
-            } else if character.eq(&'\n') {
-                indicators.push('\n'.to_string())
-            } else if character.eq(&'!') {
-                indicators.push(":exclamation: ".to_string());
-            } else if character.eq(&'?') {
-                indicators.push(":question: ".to_string());
+            match character as u8 - '0' as u8 {
+                0 => number = "zero",
+                1 => number = "one",
+                2 => number = "two",
+                3 => number = "three",
+                4 => number = "four",
+                5 => number = "five",
+                6 => number = "six",
+                7 => number = "seven",
+                8 => number = "eight",
+                9 => number = "nine",
+                _ => panic!(), // Unreachable
             }
+
+            indicators.push(format!(":{number}: "));
+        } else if character.is_whitespace() && !character.eq(&'\n') {
+            indicators.push(" ".to_string());
+        } else if character.eq(&'\n') {
+            indicators.push('\n'.to_string())
+        } else if character.eq(&'!') {
+            indicators.push(":exclamation: ".to_string());
+        } else if character.eq(&'?') {
+            indicators.push(":question: ".to_string());
         }
-
-        println!("{}", indicators.join("").trim_end());
-
-        ExitCode::SUCCESS
-    } else {
-        ExitCode::FAILURE
     }
+
+    println!("{}", indicators.join("").trim_end())
 }
