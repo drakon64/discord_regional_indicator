79c35cf92ee70c584776732a5c23f8b6eb0d147a
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-06-27 21:12:50 +0100

    Fix number handling and add exit codes

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,53 @@
 use std::env::args;
+use std::process::ExitCode;
 
-fn main() {
-    let binding = args().skip(1).next().unwrap();
-    let chars = binding.chars();
+fn main() -> ExitCode {
+    let args = args().skip(1).next();
 
-    let mut indicators = Vec::new();
+    if args.is_some() {
+        let binding = args.unwrap();
 
-    for indicator in chars {
-        if indicator.is_ascii_alphabetic() {
-            indicators.push(format!(
-                ":regional_indicator_{}: ",
-                indicator.to_lowercase()
-            ));
-        } else if indicator.is_numeric() {
-            indicators.push(format!(":{indicator}:"));
-        } else if indicator.is_whitespace() && !indicator.eq(&'\n') {
-            indicators.push(" ".to_string());
-        } else if indicator.eq(&'\n') {
-            indicators.push('\n'.to_string())
-        } else if indicator.eq(&'!') {
-            indicators.push(":exclamation: ".to_string());
-        } else if indicator.eq(&'?') {
-            indicators.push(":question: ".to_string());
+        let mut indicators = Vec::new();
+
+        for indicator in binding.chars() {
+            if indicator.is_ascii_alphabetic() {
+                indicators.push(format!(
+                    ":regional_indicator_{}: ",
+                    indicator.to_lowercase()
+                ));
+            } else if indicator.is_numeric() {
+                let number;
+
+                match indicator as u8 - '0' as u8 {
+                    0 => number = "zero",
+                    1 => number = "one",
+                    2 => number = "two",
+                    3 => number = "three",
+                    4 => number = "four",
+                    5 => number = "five",
+                    6 => number = "six",
+                    7 => number = "seven",
+                    8 => number = "eight",
+                    9 => number = "nine",
+                    _ => panic!() // Unreachable
+                }
+
+                indicators.push(format!(":{number}: "));
+            } else if indicator.is_whitespace() && !indicator.eq(&'\n') {
+                indicators.push(" ".to_string());
+            } else if indicator.eq(&'\n') {
+                indicators.push('\n'.to_string())
+            } else if indicator.eq(&'!') {
+                indicators.push(":exclamation: ".to_string());
+            } else if indicator.eq(&'?') {
+                indicators.push(":question: ".to_string());
+            }
         }
-    }
 
-    println!("{}", indicators.join("").trim_end());
+        println!("{}", indicators.join("").trim_end());
+
+        ExitCode::SUCCESS
+    } else {
+        ExitCode::FAILURE
+    }
 }
